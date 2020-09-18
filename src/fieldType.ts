6643efa82aaf8a768feda8f82857581059da738f
Waldemar Woźniak <wozniakvsc@gmail.com>
2020-09-18 15:35:16 +0200

    u4

--- src/fieldType.ts
+++ src/fieldType.ts
@@ -1,5 +1,6 @@
+import { TextareaField } from './textAreaField';
 // enumerator FieldType który będzie definiował typy pól możliwych do użycia w ankiecie (pole tekstowe, pole wielolinijkowe, data, e-mail, pole wyboru, checkbox)
 
 export enum FieldType {
-  Text = "TEXT", MultiLineText = "MULTILINETEXT", Date = "DATE", Email = "EMAIL", Select = "SELECT", Checkbox = "CHECKBOX"
+  Text = "TEXT", MultiLineText = "MULTILINETEXT", Date = "DATE", Email = "EMAIL", Select = "SELECT", Checkbox = "CHECKBOX", TextareaField= "TextArea"
 }
---
+++ src/router.ts
@@ -0,0 +1,3 @@
+const query: string = window.location.search.substr(0))
+const urlParams = new URLSearchParams(query);
+const id = urlParams.get(‘id’);
